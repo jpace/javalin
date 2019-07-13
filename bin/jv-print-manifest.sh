e317f5933c91397fe8bc6dc3bf52a0666168b663
Jeff Pace <jpace@softwareag.com>
2019-07-13 14:44:04 -0400

    Add print-manifest

--- bin/jv-print-manifest.sh
+++ bin/jv-print-manifest.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-unzip -c $* META-INF/MANIFEST.MF | perl -0777 -pe 's/\r?\n //msg;'
+# -*- shell-mode -*-
+
+for i in $*
+do
+    echo $i
+    unzip -c $i META-INF/MANIFEST.MF | perl -0777 -pe 's/\r?\n //msg;'
+done
